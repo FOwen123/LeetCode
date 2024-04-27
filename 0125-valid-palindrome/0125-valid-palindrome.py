aab9c9fd2269440d9b8f976b8e460b2c76085c32
Filbert Owen <144812376+FOwen123@users.noreply.github.com>
2024-04-27 11:32:23 +0800

    Time: 42 ms (73.44%), Space: 17 MB (63.18%) - LeetHub

---
+++ 0125-valid-palindrome/0125-valid-palindrome.py
@@ -0,0 +1,14 @@
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        temp = ""
+        for i in s.lower():
+            if i.isalnum():
+                temp += i
+
+        return temp == temp[::-1]
+
+
+        
+
+
+        
