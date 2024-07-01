c3c648fad695ef042ccd0d7abdc21dec67ad96dc
Filbert Owen <144812376+FOwen123@users.noreply.github.com>
2024-07-01 12:52:50 +0800

    Time: 707 ms (53.03%), Space: 19.8 MB (99.24%) - LeetHub

--- 0015-3sum/0015-3sum.py
+++ 0015-3sum/0015-3sum.py
@@ -5,6 +5,9 @@ class Solution:
         ans = set()
         
         for i in range(len(nums)):
+            if i > 0 and nums[i] == nums[i - 1]:
+                continue  # Skip duplicates
+                
             left = i + 1
             right = len(nums) - 1
             while left < right:
@@ -15,6 +18,10 @@ class Solution:
                     right -= 1
                 else:
                     ans.add((nums[i], nums[left], nums[right]))
+                    while left < right and nums[left] == nums[left + 1]:
+                        left += 1  # Skip duplicates
+                    while left < right and nums[right] == nums[right - 1]:
+                        right -= 1  # Skip duplicates
                     left += 1
                     right -= 1
                     
